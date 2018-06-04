f01332b1638c3b0d0a0129caaeef01c9e12ea33d
Ivo-Schwizer <ivo@schwizerschlatter.com>
2018-06-04 13:18:01 +0200

    test php header

---
+++ header.php
@@ -0,0 +1,1 @@
+<h1>ich bin die Navigation</h1>
---
+++ index.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="de-CH">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Advokatur und Treuhand am Falkenstein</title>
+    <meta name="robots" content="all">
+    <meta name="robots" content="index, follow">
+    <meta name="author" content="Solarius Rosenberg">
+    <meta name="description" content="..."><!-- definieren -->
+    <meta name="keywords" content="Advokatur, Treuhand, Immobilien, Falkenstein, Frank Th. Petermann, Rolf W. Rempfler, Christa Rempfler, Debora Bilgeri, Andres Thürlimann, Tanja Ivanovic, Werner Rempfler, Maria Rempfler, Roger Schai,">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-title" content="Falkenstein.ag">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0,">
+    <meta name="theme-color" content="#ffffff">
+    <meta property="og:type" content="business.business">
+    <meta property="og:title" content="Advokatur und Treuhand am Falkenstein">
+    <meta property="og:url" content="http://falkenstein.ag">
+    <meta property="og:image" content="..."><!-- definieren -->
+    <meta property="business:contact_data:street_address" content="Falkensteinstrasse 1">
+    <meta property="business:contact_data:locality" content="St.Gallen">
+    <meta property="business:contact_data:region" content="9016">
+    <meta property="business:contact_data:postal_code" content="9016">
+    <meta property="business:contact_data:country_name" content="Switzerland">
+    <link rel="apple-touch-icon" href="icon.png">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link href="https://fonts.googleapis.com/css?family=PT+Sans|PT+Serif" rel="stylesheet">
+</head>
+
+<body>
+    <?php include '/header.php'; ?>
+
+  
+  <p>Hello world! This is HTML5 Boilerplate.</p>
+    
+  <script src="js/vendor/modernizr-3.6.0.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+  <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.3.1.min.js"><\/script>')</script>
+  <script src="js/plugins.js"></script>
+  <script src="js/main.js"></script>
+ 
+</body>
+
+</html>
