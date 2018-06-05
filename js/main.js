0a69e52ac273c40c9da2255f4dc486eef6005137
Ivo-Schwizer <ivo@schwizerschlatter.com>
2018-06-05 15:03:39 +0200

    phptest

--- js/main.js
+++ js/main.js
@@ -10,6 +10,33 @@ $(document).ready(function () {
         $('.dropdown-nav').stop(true, true).slideUp('slow');
     });
     
+    // Accordion
+    
+    $('.accordionButton').click(function() {
+       $(this).children('.accordion-list')stop(true, true).show('slow'); 
+    });
     
     
+    /*
+    $('.accordionButton').click(function() {
+        event.preventDefault();
+        $(this).toggleClass('openred');
+        $accordion_title = $(this);
+        $accordion_content = $(this).next('.accordion-list');
+        $('.accordion-list').not($accordion_content).slideUp();
+        $('.accordion-list').not($accordion_content).prev('.accordionButton').removeClass('openred');
+        $accordion_content.stop(true, true).slideToggle(400);
+        if ($(this).hasClass('openred')) {
+          ScrollToTop();
+    }
+    });
+
+    function ScrollToTop() {
+      setTimeout(function() {
+        $('html, body').animate({
+          scrollTop: $($accordion_title).offset().top - 20
+        }, 600);
+      }, 600);
+    }
+    */
 });
