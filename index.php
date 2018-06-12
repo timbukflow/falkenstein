2b59e5eed44e418bf871ed6e0ccfb0a02dd68f23
Ivo-Schwizer <ivo@schwizerschlatter.com>
2018-06-12 19:26:23 +0200

    Picture upload and index

--- index.php
+++ index.php
@@ -59,82 +59,126 @@
             <img alt="advokatur-treuhand-falkenstein-logo" src="img/advokatur-treuhand-falkenstein-logo.svg">
         </div>
         <article>
+            <a class="content-nav column-two" href="frank-th-petermann" target="_top">
+                <div class="container-img">
+                    <img class="img1" src="img/falkenstein-advokatur-frank-petermann.jpg" alt="falkenstein-advokatur-frank-petermann" >
+                    <img class="img2" src="img/falkenstein-advokatur-frank-petermann-buch.jpg" alt="falkenstein-advokatur-frank-petermann-buch" >
+                </div>
+                <h3 class="nav">
+                    <div class="line">&#x0007C;</div>Frank Th. Petermann<span class="marking">Advokatur</span>
+                </h3>
+            </a>
+            <a class="content-nav column-two" href="rolf-w-rempfler" target="_top">
+                <div class="container-img">
+                    <img class="img1" src="img/falkenstein-advokatur-rolf-rempfler.jpg" alt="falkenstein-advokatur-rolf-rempfler" >
+                    <img class="img2" src="img/falkenstein-advokatur-rolf-rempfler-tennis.jpg" alt="falkenstein-advokatur-rolf-rempfler-tennis" >
+                </div>
+                <h3 class="nav">
+                    <div class="line">&#x0007C;</div>Rolf W. Rempfler<span class="marking">Advokatur</span>
+                </h3>
+            </a>
+            <a class="content-nav column-two" href="christa-rempfler" target="_top">
+                <div class="container-img">
+                    <img class="img1" src="img/falkenstein-advokatur-christa-rempfler.jpg" alt="falkenstein-advokatur-christa-rempfler" >
+                    <img class="img2" src="img/falkenstein-advokatur-christa-rempfler-noten.jpg" alt="falkenstein-advokatur-christa-rempfler-noten" >
+                </div>
+                <h3 class="nav">
+                    <div class="line">&#x0007C;</div>Christa Rempfler<span class="marking">Advokatur</span>
+                </h3>
+            </a>
+            <a class="content-nav column-two" href="debora-bilgeri" target="_top">
+                <div class="container-img">
+                    <img class="img1" src="img/falkenstein-advokatur-debora-bilgeri.jpg" alt="falkenstein-advokatur-debora-bilgeri" >
+                    <img class="img2" src="img/falkenstein-advokatur-debora-bilgeri-trekking.jpg" alt="falkenstein-advokatur-debora-bilgeri-trekking" >
+                </div>
+                <h3 class="nav">
+                    <div class="line">&#x0007C;</div>Debora Bilgeri<span class="marking">Advokatur</span>
+                </h3>
+            </a>
+            <a class="content-nav column-two" href="andres-thuerlimann" target="_top">
+                <div class="container-img">
+                    <img class="img1" src="img/falkenstein-advokatur-andres-thuerlimann.jpg" alt="falkenstein-advokatur-andres-thuerlimann" >
+                    <img class="img2" src="img/falkenstein-advokatur-andres-thuerlimann-koch.jpg" alt="falkenstein-advokatur-andres-thuerlimann-koch" >
+                </div>
+                <h3 class="nav">
+                    <div class="line">&#x0007C;</div>Andres Thürlimann<span class="marking">Advokatur</span>
+                </h3>
+            </a>
+            <a class="content-nav column-two" href="tanja-ivanovic" target="_top">
+                <div class="container-img">
+                    <img class="img1" src="img/falkenstein-advokatur-tanja-ivanovic.jpg" alt="falkenstein-advokatur-tanja-ivanovic" >
+                    <img class="img2" src="img/falkenstein-advokatur-tanja-ivanovic-welt.jpg" alt="falkenstein-advokatur-tanja-ivanovic-welt" >
+                </div>
+                <h3 class="nav">
+                    <div class="line">&#x0007C;</div>Tanja Ivanovic<span class="marking">Advokatur</span>
+                </h3>
+            </a>
+            <section class="content-right column-four">
+                <h1><span>Ein Mandat ist für uns nicht einfach ein Auftrag.</span><br>Es ist der Entscheid, mit einem Klienten wenn nötig bis zu den Grenzen des Möglichen vorzustossen. Die dazu nötigen Schritte erfüllen wir sorgfältig und mit hoher Professionalität. Wir gewährleisten eine fachlich herausragende Qualität, wofür wir eine angemessene Honorierung erwarten.</h1>
+            </section>
+            <a class="content-nav column-two" href="werner-rempfler" target="_top">
+                <div class="container-img">
+                    <img class="img1" src="img/falkenstein-treuhand-werner-rempfler.jpg" alt="falkenstein-treuhand-werner-rempfler" >
+                    <img class="img2" src="img/falkenstein-treuhand-werner-rempfler-jagd.jpg" alt="falkenstein-treuhand-werner-rempfler-jagd" >
+                </div>
+                <h3 class="nav">
+                    <div class="line">&#x0007C;</div>Werner Rempfler<span class="marking">Immobilien</span>
+                </h3>
+            </a>
+            <a class="content-nav column-two" href="maria-rempfler" target="_top">
+                <div class="container-img">
+                    <img class="img1" src="img/falkenstein-immobilien-maria-rempfler.jpg" alt="falkenstein-immobilien-maria-rempfler" >
+                    <img class="img2" src="img/falkenstein-immobilien-maria-rempfler-jass.jpg" alt="falkenstein-immobilien-maria-rempfler-jass" >
+                </div>
+                <h3 class="nav">
+                    <div class="line">&#x0007C;</div>Maria Rempfler<span class="marking">Immobilien</span>
+                </h3>
+            </a>
             <a class="content-nav column-two" href="roger-schai" target="_top">
                 <div class="container-img">
                     <img class="img1" src="img/falkenstein-treuhand-roger-schai.jpg" alt="falkenstein-treuhand-roger-schai" >
-                    <img class="img2" src="img/falkenstein-treuhand-roger-schai-fussball.jpg" alt="falkenstein-treuhand-roger-schai" >
+                    <img class="img2" src="img/falkenstein-treuhand-roger-schai-fussball.jpg" alt="falkenstein-treuhand-roger-schai-fussball" >
                 </div>
                 <h3 class="nav">
-                    <div class="line">&#x0007C;</div><span class="hover-nav">Roge Schai</span>
+                    <div class="line">&#x0007C;</div>Roger Schai<span class="marking">Treuhand</span>
                 </h3>
             </a>
-            <a class="content-nav column-two" href="index.html" target="_top">
+            <a class="content-nav column-two" href="fabienne-frei" target="_top">
                 <div class="container-img">
-                    <img class="img1" src="img/falkenstein-advokatur-frank-th-petermann-business.jpg" alt="frank" >
-                    <img class="img2" src="img/falkenstein-advokatur-frank-th-petermann-passion.jpg" alt="frank" >
+                    <img class="img1" src="img/falkenstein-sekretariat-fabienne-frei.jpg" alt="falkenstein-sekretariat-fabienne-frei" >
+                    <img class="img2" src="img/falkenstein-sekretariat-fabienne-frei-fotografie.jpg" alt="falkenstein-sekretariat-fabienne-frei-fotografie" >
                 </div>
                 <h3 class="nav">
-                    <div class="line">&#x0007C;</div><span class="hover-nav">Frank Th. Petermann</span>
+                    <div class="line">&#x0007C;</div>Fabienne Frei<span class="marking">Sekretariat</span>
                 </h3>
             </a>
-            <a class="content-nav column-two" href="index.html" target="_top">
+            <a class="content-nav column-two" href="franziska-sutter" target="_top">
                 <div class="container-img">
-                    <img class="img1" src="img/falkenstein-advokatur-frank-th-petermann-business.jpg" alt="frank" >
-                    <img class="img2" src="img/falkenstein-advokatur-frank-th-petermann-passion.jpg" alt="frank" >
+                    <img class="img1" src="img/falkenstein-sekretariat-franziska-sutter.jpg" alt="falkenstein-sekretariat-franziska-sutter" >
+                    <img class="img2" src="img/falkenstein-sekretariat-franziska-sutter-wandern.jpg" alt="falkenstein-sekretariat-franziska-sutter-wandern" >
                 </div>
                 <h3 class="nav">
-                    <div class="line">&#x0007C;</div><span class="hover-nav">Frank Th. Petermann</span>
+                    <div class="line">&#x0007C;</div>Franziska Sutter<span class="marking">Sekretariat</span>
                 </h3>
             </a>
-            <a class="content-nav column-two" href="index.html" target="_top">
-                <div class="container-img">
-                    <img class="img1" src="img/falkenstein-advokatur-frank-th-petermann-business.jpg" alt="frank" >
-                    <img class="img2" src="img/falkenstein-advokatur-frank-th-petermann-passion.jpg" alt="frank" >
-                </div>
-                <h3 class="nav">
-                    <div class="line">&#x0007C;</div><span class="hover-nav">Frank Th. Petermann</span>
-                </h3>
-            </a>
-            <a class="content-nav column-two" href="index.html" target="_top">
-                <div class="container-img">
-                    <img class="img1" src="img/falkenstein-advokatur-frank-th-petermann-business.jpg" alt="frank" >
-                    <img class="img2" src="img/falkenstein-advokatur-frank-th-petermann-passion.jpg" alt="frank" >
-                </div>
-                <h3 class="nav">
-                    <div class="line">&#x0007C;</div><span class="hover-nav">Frank Th. Petermann</span>
-                </h3>
-            </a>
-            <a class="content-nav column-two" href="index.html" target="_top">
-                <div class="container-img">
-                    <img class="img1" src="img/falkenstein-advokatur-frank-th-petermann-business.jpg" alt="frank" >
-                    <img class="img2" src="img/falkenstein-advokatur-frank-th-petermann-passion.jpg" alt="frank" >
-                </div>
-                <h3 class="nav">
-                    <div class="line">&#x0007C;</div><span class="hover-nav">Frank Th. Petermann</span>
-                </h3>
-            </a>
-            <section class="content-left column-five">
-                <h1><span>Ein Mandat ist für uns nicht einfach ein Auftrag.</span><br>Es ist der Entscheid, mit einem Klienten wenn nötig bis zu den Grenzen des Möglichen vorzustossen. Die dazu nötigen Schritte erfüllen wir sorgfältig und mit hoher Professionalität. Wir gewährleisten eine fachlich herausragende Qualität, wofür wir eine angemessene Honorierung erwarten.</h1>
-            </section>
-            <section class="content-left column-five">
+            <section class="content-right column-four">
                 <h1><span>Nicht alles, was Tradition hat, ist sinnvoll.</span><br>Liberal und aufgeklärt stehen wir Forderungen, wie dem "Rückbesinnen auf alte Werte" skeptisch gegenüber. Wir lehnen Tradition jedoch nicht grundsätzlich ab. Das Traditionelle Taekwon-Do, welches auf den Eigenschaften jahrtausendealter asiatischer Kampfkünste aufbaut, beruht auf fünf Grundwerten, an denen auch wir unser Wirken ausrichten:</h1>
             </section>
         </article>
         
-        <article class="column-two accordion">
+        <article class="content-right column-four accordion">
             <h3 class="nav accTitle"><div class="line">&#x0007C;</div>Respekt</h3>
             <p class="accList">Wir pflegen untereinander und mit allen unseren Ansprechpartnern einen höflichen und respektvollen Umgang. Davon weichen wir auch dann nicht ab, wenn wir einen Standpunkt oder eine Entscheidung gegen Widerstand zu vertreten haben. Von unseren Klienten erwarten wir das gleiche.</p>
             
             <h3 class="nav accTitle"><div class="line">&#x0007C;</div>Ausdauer</h3>
             <p class="accList">Verfahren können mitunter lange dauern und belastend sein. Manchmal hat man auf dem Weg zum Ziel auch Rückschläge hinzunehmen. Das hindert uns nicht daran, auch lange und belastende Prozesse mit Geduld, Umsicht und Beharrlichkeit zu führen.</p>
-        </article>  
-        <article class="column-two accordion">    
+            
             <h3 class="nav accTitle"><div class="line">&#x0007C;</div>Selbstdisziplin</h3>
             <p class="accList">Kontinuierlich reflektieren wir uns und unser Handeln. Da wir uns dem Unterschied zwischen den Interessen unserer Klienten und unserer eigenen bewusst sind, und unser Handeln nach diesem Bewusstsein ausrichten, kommt es nicht zu Interessenkonflikten. Impulsivität entspricht nicht unseren Vorstellungen einer professionellen Arbeitsweise. Wir handeln ruhig und überlegt.</p>
             
             <h3 class="nav accTitle"><div class="line">&#x0007C;</div>Unbeugsamer Geist</h3>
             <p class="accList">Wir prüfen auch unkonventionelle Lösungen, bevor wir Verfahren anhängig machen. Ist aber ein Konflikt unvermeidbar, vertreten wir unsere Klienten mit Engagement und Standfestigkeit gegenüber der Gegenpartei, Gerichten oder Verwaltungsbehörden. Und wenn nötig auch mit einer gehörigen Portion Renitenz.</p>
-        </article>
+        </article> 
     </main>
     
     <?php include 'include/footer.php'; ?>
