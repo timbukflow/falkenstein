de62ab71cce4f21ecc906f5d9d2e3aa9d158fa20
timbuk <ivoschwizer@gmail.com>
2018-06-13 13:58:49 +0200

    favicon

---
+++ include/head.php
@@ -0,0 +1,44 @@
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Advokatur und Treuhand am Falkenstein</title>
+    <meta name="robots" content="all">
+    <meta name="robots" content="index, follow">
+    <meta name="author" content="Advokatur und Treuhand am Falkenstein">
+    <meta name="description" content="..."><!-- definieren -->
+    <meta name="keywords" content="Advokatur, Treuhand, Immobilien, Falkenstein, Frank Th. Petermann, Rolf W. Rempfler, Christa Rempfler, Debora Bilgeri, Andres Thürlimann, Tanja Ivanovic, Werner Rempfler, Maria Rempfler, Roger Schai,">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-title" content="Falkenstein.ag">
+    <meta name="viewport" content="initial-scale=1.0">
+    <meta property="og:type" content="business.business">
+    <meta property="og:title" content="Advokatur und Treuhand am Falkenstein">
+    <meta property="og:url" content="http://falkenstein.ag">
+    <meta property="og:image" content="..."><!-- definieren Bild von Haus -->
+    <meta property="business:contact_data:street_address" content="Falkensteinstrasse 1">
+    <meta property="business:contact_data:locality" content="St.Gallen">
+    <meta property="business:contact_data:region" content="9016">
+    <meta property="business:contact_data:postal_code" content="9016">
+    <meta property="business:contact_data:country_name" content="Switzerland">
+    
+    <link rel="apple-touch-icon" sizes="57x57" href="/favicon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="/favicon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="/favicon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="/favicon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="/favicon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="/favicon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="/favicon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="/favicon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="/favicon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="/favicon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/favicon/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="/favicon/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+    
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link href="https://fonts.googleapis.com/css?family=PT+Sans" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=PT+Serif" rel="stylesheet">
