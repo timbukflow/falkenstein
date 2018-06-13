2609572e89c90474124bb796d32c8aadc2da6054
timbuk <ivoschwizer@gmail.com>
2018-06-13 14:55:56 +0200

    W3C index fix

--- index.php
+++ index.php
@@ -31,7 +31,7 @@
                     <img class="img2" src="img/falkenstein-advokatur-rolf-rempfler-tennis.jpg" alt="falkenstein-advokatur-rolf-rempfler-tennis" >
                 </div>
                 <h3 class="nav">
-                    <div class="line">&#x0007C;</div>Rolf W. Rempfler <span class="gray">Advokatur</span>
+                    <span class="line">&#x0007C;</span>Rolf W. Rempfler <span class="gray">Advokatur</span>
                 </h3>
             </a>
             <a class="content-nav column-two" href="christa-rempfler" target="_top">
@@ -40,7 +40,7 @@
                     <img class="img2" src="img/falkenstein-advokatur-christa-rempfler-noten.jpg" alt="falkenstein-advokatur-christa-rempfler-noten" >
                 </div>
                 <h3 class="nav">
-                    <div class="line">&#x0007C;</div>Christa Rempfler <span class="gray">Advokatur</span>
+                    <span class="line">&#x0007C;</span>Christa Rempfler <span class="gray">Advokatur</span>
                 </h3>
             </a>
             <a class="content-nav column-two" href="debora-bilgeri" target="_top">
@@ -49,7 +49,7 @@
                     <img class="img2" src="img/falkenstein-advokatur-debora-bilgeri-trekking.jpg" alt="falkenstein-advokatur-debora-bilgeri-trekking" >
                 </div>
                 <h3 class="nav">
-                    <div class="line">&#x0007C;</div>Debora Bilgeri <span class="gray">Advokatur</span>
+                    <span class="line">&#x0007C;</span>Debora Bilgeri <span class="gray">Advokatur</span>
                 </h3>
             </a>
             <a class="content-nav column-two" href="andres-thuerlimann" target="_top">
@@ -58,7 +58,7 @@
                     <img class="img2" src="img/falkenstein-advokatur-andres-thuerlimann-koch.jpg" alt="falkenstein-advokatur-andres-thuerlimann-koch" >
                 </div>
                 <h3 class="nav">
-                    <div class="line">&#x0007C;</div>Andres Thürlimann <span class="gray">Advokatur</span>
+                    <span class="line">&#x0007C;</span>Andres Thürlimann <span class="gray">Advokatur</span>
                 </h3>
             </a>
             <a class="content-nav column-two" href="tanja-ivanovic" target="_top">
@@ -67,11 +67,11 @@
                     <img class="img2" src="img/falkenstein-advokatur-tanja-ivanovic-welt.jpg" alt="falkenstein-advokatur-tanja-ivanovic-welt" >
                 </div>
                 <h3 class="nav">
-                    <div class="line">&#x0007C;</div>Tanja Ivanovic <span class="gray">Advokatur</span>
+                    <span class="line">&#x0007C;</span>Tanja Ivanovic <span class="gray">Advokatur</span>
                 </h3>
             </a>
             <section class="content-right column-four">
-                <h1><span>Ein Mandat ist für uns nicht einfach ein Auftrag.</span><br>Es ist der Entscheid, mit einem Klienten wenn nötig bis zu den Grenzen des Möglichen vorzustossen. Die dazu nötigen Schritte erfüllen wir sorgfältig und mit hoher Professionalität. Wir gewährleisten eine fachlich herausragende Qualität, wofür wir eine angemessene Honorierung erwarten.</h1>
+                <h2><span>Ein Mandat ist für uns nicht einfach ein Auftrag.</span><br>Es ist der Entscheid, mit einem Klienten wenn nötig bis zu den Grenzen des Möglichen vorzustossen. Die dazu nötigen Schritte erfüllen wir sorgfältig und mit hoher Professionalität. Wir gewährleisten eine fachlich herausragende Qualität, wofür wir eine angemessene Honorierung erwarten.</h2>
             </section>
             <a class="content-nav column-two" href="werner-rempfler" target="_top">
                 <div class="container-img">
@@ -79,7 +79,7 @@
                     <img class="img2" src="img/falkenstein-treuhand-werner-rempfler-jagd.jpg" alt="falkenstein-treuhand-werner-rempfler-jagd" >
                 </div>
                 <h3 class="nav">
-                    <div class="line">&#x0007C;</div>Werner Rempfler <span class="gray">Immobilien</span>
+                    <span class="line">&#x0007C;</span>Werner Rempfler <span class="gray">Immobilien</span>
                 </h3>
             </a>
             <a class="content-nav column-two" href="maria-rempfler" target="_top">
@@ -88,7 +88,7 @@
                     <img class="img2" src="img/falkenstein-immobilien-maria-rempfler-jass.jpg" alt="falkenstein-immobilien-maria-rempfler-jass" >
                 </div>
                 <h3 class="nav">
-                    <div class="line">&#x0007C;</div>Maria Rempfler <span class="gray">Immobilien</span>
+                    <span class="line">&#x0007C;</span>Maria Rempfler <span class="gray">Immobilien</span>
                 </h3>
             </a>
             <a class="content-nav column-two" href="roger-schai" target="_top">
@@ -97,7 +97,7 @@
                     <img class="img2" src="img/falkenstein-treuhand-roger-schai-fussball.jpg" alt="falkenstein-treuhand-roger-schai-fussball" >
                 </div>
                 <h3 class="nav">
-                    <div class="line">&#x0007C;</div>Roger Schai <span class="gray">Treuhand</span>
+                    <span class="line">&#x0007C;</span>Roger Schai <span class="gray">Treuhand</span>
                 </h3>
             </a>
             <a class="content-nav column-two" href="fabienne-frei" target="_top">
@@ -106,7 +106,7 @@
                     <img class="img2" src="img/falkenstein-sekretariat-fabienne-frei-fotografie.jpg" alt="falkenstein-sekretariat-fabienne-frei-fotografie" >
                 </div>
                 <h3 class="nav">
-                    <div class="line">&#x0007C;</div>Fabienne Frei <span class="gray">Sekretariat</span>
+                    <span class="line">&#x0007C;</span>Fabienne Frei <span class="gray">Sekretariat</span>
                 </h3>
             </a>
             <a class="content-nav column-two" href="franziska-sutter" target="_top">
@@ -115,25 +115,25 @@
                     <img class="img2" src="img/falkenstein-sekretariat-franziska-sutter-wandern.jpg" alt="falkenstein-sekretariat-franziska-sutter-wandern" >
                 </div>
                 <h3 class="nav">
-                    <div class="line">&#x0007C;</div>Franziska Sutter <span class="gray">Sekretariat</span>
+                    <span class="line">&#x0007C;</span>Franziska Sutter <span class="gray">Sekretariat</span>
                 </h3>
             </a>
             <section class="content-right column-four">
-                <h1><span>Nicht alles, was Tradition hat, ist sinnvoll.</span><br>Liberal und aufgeklärt stehen wir Forderungen, wie dem "Rückbesinnen auf alte Werte" skeptisch gegenüber. Wir lehnen Tradition jedoch nicht grundsätzlich ab. Das Traditionelle Taekwon-Do, welches auf den Eigenschaften jahrtausendealter asiatischer Kampfkünste aufbaut, beruht auf fünf Grundwerten, an denen auch wir unser Wirken ausrichten:</h1>
+                <h2><span>Nicht alles, was Tradition hat, ist sinnvoll.</span><br>Liberal und aufgeklärt stehen wir Forderungen, wie dem "Rückbesinnen auf alte Werte" skeptisch gegenüber. Wir lehnen Tradition jedoch nicht grundsätzlich ab. Das Traditionelle Taekwon-Do, welches auf den Eigenschaften jahrtausendealter asiatischer Kampfkünste aufbaut, beruht auf fünf Grundwerten, an denen auch wir unser Wirken ausrichten:</h2>
             </section>
         </article>
         
         <article class="content-right column-four accordion">
-            <h3 class="nav accTitle"><div class="line">&#x0007C;</div>Respekt</h3>
+            <h3 class="nav accTitle"><span class="line">&#x0007C;</span>Respekt</h3>
             <p class="accList">Wir pflegen untereinander und mit allen unseren Ansprechpartnern einen höflichen und respektvollen Umgang. Davon weichen wir auch dann nicht ab, wenn wir einen Standpunkt oder eine Entscheidung gegen Widerstand zu vertreten haben. Von unseren Klienten erwarten wir das gleiche.</p>
             
-            <h3 class="nav accTitle"><div class="line">&#x0007C;</div>Ausdauer</h3>
+            <h3 class="nav accTitle"><span class="line">&#x0007C;</span>Ausdauer</h3>
             <p class="accList">Verfahren können mitunter lange dauern und belastend sein. Manchmal hat man auf dem Weg zum Ziel auch Rückschläge hinzunehmen. Das hindert uns nicht daran, auch lange und belastende Prozesse mit Geduld, Umsicht und Beharrlichkeit zu führen.</p>
             
-            <h3 class="nav accTitle"><div class="line">&#x0007C;</div>Selbstdisziplin</h3>
+            <h3 class="nav accTitle"><span class="line">&#x0007C;</span>Selbstdisziplin</h3>
             <p class="accList">Kontinuierlich reflektieren wir uns und unser Handeln. Da wir uns dem Unterschied zwischen den Interessen unserer Klienten und unserer eigenen bewusst sind, und unser Handeln nach diesem Bewusstsein ausrichten, kommt es nicht zu Interessenkonflikten. Impulsivität entspricht nicht unseren Vorstellungen einer professionellen Arbeitsweise. Wir handeln ruhig und überlegt.</p>
             
-            <h3 class="nav accTitle"><div class="line">&#x0007C;</div>Unbeugsamer Geist</h3>
+            <h3 class="nav accTitle"><span class="line">&#x0007C;</span>Unbeugsamer Geist</h3>
             <p class="accList">Wir prüfen auch unkonventionelle Lösungen, bevor wir Verfahren anhängig machen. Ist aber ein Konflikt unvermeidbar, vertreten wir unsere Klienten mit Engagement und Standfestigkeit gegenüber der Gegenpartei, Gerichten oder Verwaltungsbehörden. Und wenn nötig auch mit einer gehörigen Portion Renitenz.</p>
         </article> 
     </main>
