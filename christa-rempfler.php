0ec4a33c856a3e9eaeeb4469c675839b5157abea
timbuk <ivoschwizer@gmail.com>
2018-06-14 08:01:37 +0200

    W3C section fix

--- christa-rempfler.php
+++ christa-rempfler.php
@@ -47,18 +47,15 @@
             <p class="accList">Advokatur am Falkenstein<br>Christa Rempfler<br>Falkensteinstrasse 1<br>Postfach 152<br>9016 St. Gallen<br><br><a class="nummer" href="tel:0712426653">T: 071 242 66 53</a>F: 071 242 66 52<a class="nummer" href="mailto:cr@falkenstein.ag" target="_top">cr@falkenstein.ag</a><br><span class="gray">PC-Konto: 85-361451-6<br>IBAN: CH10 0900 0000 8536 1451 6<br>MwSt.: CHE-452.133.484 MWST</span></p>
         </article>  
         
-        
-        <section>
-            <div class="column-three container-img content-team-img">
-                <img class="img1" src="img/falkenstein-advokatur-christa-rempfler.jpg" alt="falkenstein-advokatur-christa-rempfler" >
-            </div>
-            <div class="column-three container-img content-nav">
-                <img class="img1" src="img/falkenstein-advokatur-christa-rempfler-noten.jpg" alt="falkenstein-advokatur-christa-rempfler-noten" >
-            </div>
-            <article class="content-right column-four">
-                <h2>&laquo; Liberal und aufgeklärt stehen wir Forderungen, wie dem "Rückbesinnen auf alte Werte" skeptisch gegenüber. Wir lehnen Tradition jedoch nicht grundsätzlich ab. Das Traditionelle Taekwon-Do, welches auf den Eigenschaften jahrtausendealter asiatischer Kampfkünste aufbaut, beruht auf fünf Grundwerten, an denen auch wir unser Wirken ausrichten &raquo;</h2>
-            </article>
-        </section>
+        <div class="column-three container-img content-team-img">
+            <img class="img1" src="img/falkenstein-advokatur-christa-rempfler.jpg" alt="falkenstein-advokatur-christa-rempfler" >
+        </div>
+        <div class="column-three container-img content-nav">
+            <img class="img1" src="img/falkenstein-advokatur-christa-rempfler-noten.jpg" alt="falkenstein-advokatur-christa-rempfler-noten" >
+        </div>
+        <article class="content-right column-four">
+            <h2>&laquo; Liberal und aufgeklärt stehen wir Forderungen, wie dem "Rückbesinnen auf alte Werte" skeptisch gegenüber. Wir lehnen Tradition jedoch nicht grundsätzlich ab. Das Traditionelle Taekwon-Do, welches auf den Eigenschaften jahrtausendealter asiatischer Kampfkünste aufbaut, beruht auf fünf Grundwerten, an denen auch wir unser Wirken ausrichten &raquo;</h2>
+        </article>
     </main>
     
     <?php include 'include/footer.php'; ?>
