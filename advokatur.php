3462527224b8c6b0dccaaba826e13523e61ae2fe
timbuk <ivoschwizer@gmail.com>
2018-08-15 10:52:29 +0200

    new site treuhand

--- advokatur.php
+++ advokatur.php
@@ -7,18 +7,12 @@
 
 <body>
     
-    <?php $page = 'home'; include 'include/header.php'; ?>
+    <?php $page = 'advokatur'; include 'include/header.php'; ?>
     
-    <main class="main-container">
-        <article class="content-top column-four">
-            <h1><span class="contact-resp">Advokatur und Treuhand <span class="respbr"></span>am Falkenstein</span><span class="respbr"></span><br>Wir sind eine Arbeitsgemeinschaft von individuellen Persönlichkeiten, wir arbeiten selbständig und unabhängig und pflegen einen offenen, direkten Arbeitsstil, auf der Basis umfassender und breiter Erfahrung. Loyal, hilfsbereit und absoluter Diskretion verpflichtet. Individualität ist auch der gemeinsame Nenner, den unsere Kunden miteinander gemein haben.</h1>
+    <div class="main-container">
+        <article class="content-top column-five">
+            <h1><span class="contact-resp">Advokatur <span class="respbr"></span>am Falkenstein</span><span class="respbr"></span><br>Wir sind ein engagiertes und kompetentes Team von Rechtsanwälten und Notaren, die sich für die Rechte unserer Klienten einsetzen. Wir beraten und vertreten unsere Klienten auch vor Gerichten und Verwaltungsbehörden. Spezialfragen und ausserordentliche Problemstellungen unterbreiten wir unserm Experten-Netz und holen auch ohne Prestige-Verlust eine Zweitmeinung ein.</h1>
         </article>
-        <div class="logo-container">
-            <img alt="advokatur-treuhand-falkenstein-logo" src="img/advokatur-treuhand-falkenstein-logo.svg">
-        </div>
-        <div class="logo-container-small">
-            <img class="logo-small" alt="advokatur-treuhand-falkenstein-logo-small" src="img/advokatur-treuhand-falkenstein-logo-small.svg">
-        </div>
         
         <article>
             <a class="content-nav column-two inview" href="frank-th-petermann" target="_top">
@@ -75,77 +69,11 @@
                     <span class="line">&#x0007C;</span>Tanja Ivanovic <span class="gray">Advokatur</span>
                 </h3>
             </a>
-            <article class="content-right column-four inview">
-                <h2><span>Ein Mandat ist für uns nicht <span class="respbr"></span>einfach ein Auftrag.</span><span class="respbr"></span><br>Es ist der Entscheid, mit einem Klienten wenn nötig bis zu den Grenzen des Möglichen vorzustossen. Die dazu nötigen Schritte erfüllen wir sorgfältig und mit hoher Professionalität. Wir gewährleisten eine fachlich herausragende Qualität, wofür wir eine angemessene Honorierung erwarten.</h2>
-            </article>
-            <a class="content-nav column-two inview" href="werner-rempfler" target="_top">
-                <div class="container-img">
-                    <img class="img1" src="img/falkenstein-treuhand-werner-rempfler.jpg" alt="falkenstein-treuhand-werner-rempfler" >
-                    <img class="img2" src="img/falkenstein-treuhand-werner-rempfler-jagd.jpg" alt="falkenstein-treuhand-werner-rempfler-jagd" >
-                </div>
-                <h3 class="nav">
-                    <span class="line">&#x0007C;</span>Werner Rempfler <span class="gray">Treuhand</span>
-                </h3>
-            </a>
-            <a class="content-nav column-two inview" href="roger-schai" target="_top">
-                <div class="container-img">
-                    <img class="img1" src="img/falkenstein-treuhand-roger-schai.jpg" alt="falkenstein-treuhand-roger-schai" >
-                    <img class="img2" src="img/falkenstein-treuhand-roger-schai-fussball.jpg" alt="falkenstein-treuhand-roger-schai-fussball" >
-                </div>
-                <h3 class="nav">
-                    <span class="line">&#x0007C;</span>Roger Schai <span class="gray">Treuhand</span>
-                </h3>
-            </a>
-            <a class="content-nav column-two inview" href="maria-rempfler" target="_top">
-                <div class="container-img">
-                    <img class="img1" src="img/falkenstein-immobilien-maria-rempfler.jpg" alt="falkenstein-immobilien-maria-rempfler" >
-                    <img class="img2" src="img/falkenstein-immobilien-maria-rempfler-jass.jpg" alt="falkenstein-immobilien-maria-rempfler-jass" >
-                </div>
-                <h3 class="nav">
-                    <span class="line">&#x0007C;</span>Maria Rempfler <span class="gray">Immobilien</span>
-                </h3>
-            </a>
-            <a class="content-nav column-two inview" href="franziska-sutter" target="_top">
-                <div class="container-img">
-                    <img class="img1" src="img/falkenstein-sekretariat-franziska-sutter.jpg" alt="falkenstein-sekretariat-franziska-sutter" >
-                    <img class="img2" src="img/falkenstein-sekretariat-franziska-sutter-wandern.jpg" alt="falkenstein-sekretariat-franziska-sutter-wandern" >
-                </div>
-                <h3 class="nav">
-                    <span class="line">&#x0007C;</span>Franziska Sutter <span class="gray">Sekretariat</span>
-                </h3>
-            </a>
-            <a class="content-nav column-two inview" href="fabienne-frei" target="_top">
-                <div class="container-img">
-                    <img class="img1" src="img/falkenstein-sekretariat-fabienne-frei.jpg" alt="falkenstein-sekretariat-fabienne-frei" >
-                    <img class="img2" src="img/falkenstein-sekretariat-fabienne-frei-fotografie.jpg" alt="falkenstein-sekretariat-fabienne-frei-fotografie" >
-                </div>
-                <h3 class="nav">
-                    <span class="line">&#x0007C;</span>Fabienne Frei <span class="gray">Sekretariat</span>
-                </h3>
-            </a>
-            <article class="content-left column-four inview">
-                <h2><span>Nicht alles, was Tradition hat, <span class="respbr"></span>ist sinnvoll.</span><span class="respbr"></span><br>Liberal und aufgeklärt stehen wir Forderungen, wie dem &laquo;Rückbesinnen auf alte Werte&raquo; skeptisch gegenüber. Wir lehnen Tradition jedoch nicht grundsätzlich ab. Das Traditionelle Taekwon-Do, welches auf den Eigenschaften jahrtausendealter asiatischer Kampfkünste aufbaut, beruht auf fünf Grundwerten, an denen auch wir unser Wirken ausrichten: 
-                </h2>
+            <article class="content-left column-five inview">
+                <h2><span>Unser Team in der Advokatur</span><br>deckt die Bedürfnisse und Probleme, welche sich im KMU-Bereich und in der Familie wie auch im Beruf stellen, umfassend ab. Mit interner Kooperation und Konsultation analysieren wir die Sach- und Rechtslage und finden so das adäquate Vorgehen – gerade auch unter dem Aspekt der gewaltigen Kosten, welche die Gerichte heute den Parteien auferlegen. Transparent und verständlich für den Klienten, konsequent und wenn notwendig hartnäckig gegenüber dem Gegner und den Behörden, verfolgen wir die Interessen unseres Auftraggebers.</h2>
             </article>
         </article>
-        
-        <article class="content-right-in column-four inview">
-            <h2>Respekt<br><span>Wir pflegen untereinander und mit allen unseren Ansprechpartnern einen höflichen und respektvollen Umgang. Davon weichen wir auch dann nicht ab, wenn wir einen Standpunkt oder eine Entscheidung gegen Widerstand zu vertreten haben. Von unseren Klienten erwarten wir das gleiche.</span></h2>
-        </article>
-        <article class="content-left column-four inview">
-            <h2>Ausdauer<br><span>Verfahren können mitunter lange dauern und belastend sein. Manchmal hat man auf dem Weg zum Ziel auch Rückschläge hinzunehmen. Das hindert uns nicht daran, auch lange und belastende Prozesse mit Geduld, Umsicht und Beharrlichkeit zu führen.</span></h2>
-        </article> 
-        <article class="content-right-in column-four inview">
-            <h2>Integrität<br><span>Tolerant und offen, sowie weder politisch noch an Verbände gebunden, wahren wir uns eine eigenständige Beurteilung der Sachverhalte. Mandate, die nicht mit unseren persönlichen Wertvorstellungen in Einklang stehen, übernehmen wir nicht. Wir fühlen uns unseren Kunden verpflichtet, ohne dabei auf irgendwelche gesellschaftliche Wertvorstellungen Rücksicht nehmen zu müssen.</span></h2>
-        </article> 
-        <article class="content-left column-four inview">
-            <h2>Selbstdisziplin<br><span>Kontinuierlich reflektieren wir uns und unser Handeln. Da wir uns dem Unterschied zwischen den Interessen unserer Klienten und unserer eigenen bewusst sind, und unser Handeln nach diesem Bewusstsein ausrichten, kommt es nicht zu Interessenkonflikten. Impulsivität entspricht nicht unseren Vorstellungen einer professionellen Arbeitsweise. Wir handeln ruhig und überlegt.</span></h2>
-        </article> 
-        <article class="content-right-in column-four inview">
-            <h2>Unbeugsamer Geist<br><span>Wir prüfen auch unkonventionelle Lösungen, bevor wir Verfahren anhängig machen. Ist aber ein Konflikt unvermeidbar, vertreten wir unsere Klienten mit Engagement und Standfestigkeit gegenüber der Gegenpartei, Gerichten oder Verwaltungsbehörden. Und wenn nötig auch mit einer gehörigen Portion Renitenz.</span></h2>
-        </article> 
-         
-    </main>
+    </div>
     
     <?php include 'include/footer.php'; ?>
     
---
+++ treuhand.php
@@ -0,0 +1,83 @@
+<!doctype html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="de-CH">
+<head>
+    <?php include 'include/head.php'; ?>
+    <meta name="description" content="..."><!-- definieren -->
+</head>
+
+<body>
+    
+    <?php $page = 'treuhand'; include 'include/header.php'; ?>
+    
+    <div class="main-container">
+        <article class="content-top column-five">
+            <h1><span class="contact-resp">Treuhand <span class="respbr"></span>am Falkenstein</span><span class="respbr"></span><br>Wir bieten den KMU Arbeiten an im Bereich Geschäftsgründung und Unternehmensberatung, wie: Beratung bei der Auswahl der passenden Geschäfts- und Gesellschaftsform; Firmagründung und Firmastrukturierung; Verwaltung von Sitz- und Holdinggesellschaften; Steuerliche Optimierung; Finanzierungsmodelle, Neu- und Refinanzierungen; Nachfolgeregelung, Liquidationen und Erbteilung.</h1>
+        </article>
+        
+        <article>
+            <a class="content-nav column-two inview" href="werner-rempfler" target="_top">
+                <div class="container-img">
+                    <img class="img1" src="img/falkenstein-treuhand-werner-rempfler.jpg" alt="falkenstein-treuhand-werner-rempfler" >
+                    <img class="img2" src="img/falkenstein-treuhand-werner-rempfler-jagd.jpg" alt="falkenstein-treuhand-werner-rempfler-jagd" >
+                </div>
+                <h3 class="nav">
+                    <span class="line">&#x0007C;</span>Werner Rempfler <span class="gray">Treuhand</span>
+                </h3>
+            </a>
+            <a class="content-nav column-two inview" href="roger-schai" target="_top">
+                <div class="container-img">
+                    <img class="img1" src="img/falkenstein-treuhand-roger-schai.jpg" alt="falkenstein-treuhand-roger-schai" >
+                    <img class="img2" src="img/falkenstein-treuhand-roger-schai-fussball.jpg" alt="falkenstein-treuhand-roger-schai-fussball" >
+                </div>
+                <h3 class="nav">
+                    <span class="line">&#x0007C;</span>Roger Schai <span class="gray">Treuhand</span>
+                </h3>
+            </a>
+            <a class="content-nav column-two inview" href="maria-rempfler" target="_top">
+                <div class="container-img">
+                    <img class="img1" src="img/falkenstein-immobilien-maria-rempfler.jpg" alt="falkenstein-immobilien-maria-rempfler" >
+                    <img class="img2" src="img/falkenstein-immobilien-maria-rempfler-jass.jpg" alt="falkenstein-immobilien-maria-rempfler-jass" >
+                </div>
+                <h3 class="nav">
+                    <span class="line">&#x0007C;</span>Maria Rempfler <span class="gray">Immobilien</span>
+                </h3>
+            </a>
+            <article class="content-left content-imobilien column-five inview">
+                <h2><span class="contact-resp">Immobilien <span class="respbr"></span>am Falkenstein</span><span class="respbr"></span><br>Wir bieten den KMU Arbeiten an im Bereich Geschäftsgründung und Unternehmensberatung, wie: Beratung bei der Auswahl der passenden Geschäfts- und Gesellschaftsform; Firmagründung und Firmastrukturierung; Einrichtung und Führung der Buchhaltung; Verwaltung von Sitz- und Holdinggesellschaften; Steuerliche Optimierung; Finanzierungsmodelle, Neu- und Refinanzierungen; Nachfolgeregelung, Liquidationen und Erbteilung.</h2>
+            </article>
+            
+                                                 
+            
+            <a class="content-nav column-two inview" href="franziska-sutter" target="_top">
+                <div class="container-img">
+                    <img class="img1" src="img/falkenstein-sekretariat-franziska-sutter.jpg" alt="falkenstein-sekretariat-franziska-sutter" >
+                    <img class="img2" src="img/falkenstein-sekretariat-franziska-sutter-wandern.jpg" alt="falkenstein-sekretariat-franziska-sutter-wandern" >
+                </div>
+                <h3 class="nav">
+                    <span class="line">&#x0007C;</span>Franziska Sutter <span class="gray">Sekretariat</span>
+                </h3>
+            </a>
+            <a class="content-nav column-two inview" href="fabienne-frei" target="_top">
+                <div class="container-img">
+                    <img class="img1" src="img/falkenstein-sekretariat-fabienne-frei.jpg" alt="falkenstein-sekretariat-fabienne-frei" >
+                    <img class="img2" src="img/falkenstein-sekretariat-fabienne-frei-fotografie.jpg" alt="falkenstein-sekretariat-fabienne-frei-fotografie" >
+                </div>
+                <h3 class="nav">
+                    <span class="line">&#x0007C;</span>Fabienne Frei <span class="gray">Sekretariat</span>
+                </h3>
+            </a>
+        </article>
+    </div>
+    
+    <?php include 'include/footer.php'; ?>
+    
+    
+    <script src="js/vendor/modernizr-3.6.0.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.3.1.min.js"><\/script>')</script>
+    <script src="js/plugins.js"></script>
+    <script src="js/main.js"></script>
+ 
+</body>
+
+</html>
