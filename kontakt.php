19275e3cef9f25ef5c179d0d83ded82830e0f26a
timbuk <ivoschwizer@gmail.com>
2018-08-16 11:19:47 +0200

    map

--- kontakt.php
+++ kontakt.php
@@ -16,30 +16,147 @@
                 Postfach 152<br>
                 9016 St.Gallen<br>
                 <span><a href="https://www.google.ch/maps/dir//Falkensteinstrasse+1,+9000+St.+Gallen/@47.4299966,9.3870661,15z" title="Route mit Google berechnen" target="_blank">Anfahrt mit Google</a></span>
-            </h3><br>
+            </h3>
         </section>
         <section class="content top column-two contact contact-resp">
             <h3>Advokatur<br>
                 <span class="gray"><a href="tel:0712426650">T 071 242 66 50</a>
                 F 071 242 66 52
                 <a href="mailto:info@falkenstein.ag" target="_top">info@falkenstein.ag</a></span>
-            </h3><br>
+            </h3>
         </section>
         <section class="content top column-two contact contact-resp">
             <h3>Treuhand und Immobilien<br>
                 <span class="gray"><a class="contact" href="tel:0712426657">T 071 242 66 57</a>
                 F 071 242 66 56
                 <a class="contact" href="mailto:rs@falkenstein.ag" target="_top">rs@falkenstein.ag</a></span>
-            </h3><br>
+            </h3>
         </section>
         
-        
         <div class="column-six container-img inview">
-            <img class="img1" src="img/falkenstein-advokatur-treuhand-kontakt.jpg" alt="falkenstein-advokatur-treuhand-kontakt" >
+            <div id="map"></div>
         </div>
         
     </div>
     
+   <script>
+        function initMap() {
+        var styledMapType = new google.maps.StyledMapType(
+            [{"elementType": "geometry","stylers": [{"color": "#f5f5f5"}]},
+             {"elementType": "labels.icon","stylers": [{"visibility": "off"}]},
+             {"elementType": "labels.text.fill","stylers": [{"color": "#616161"}]},
+             {"elementType": "labels.text.stroke","stylers": [{"color": "#f5f5f5"}]},
+             {"featureType": "administrative.land_parcel","elementType": "labels.text.fill","stylers": [{"color": "#bdbdbd"}]},
+             {"featureType": "poi","elementType": "geometry","stylers": [{"color": "#eeeeee"}]},{"featureType": "poi","elementType": "labels.text.fill","stylers": [{"color": "#757575"}]},
+             {"featureType": "poi.park","elementType": "geometry","stylers": [{"color": "#e5e5e5"}]},{"featureType": "poi.park","elementType": "labels.text.fill","stylers": [{"color": "#9e9e9e"}]},
+             {
+                    "featureType": "road",
+                    "elementType": "geometry",
+                    "stylers": [
+                        {
+                            "color": "#ffffff"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road.arterial",
+                    "elementType": "labels.text.fill",
+                    "stylers": [
+                        {
+                            "color": "#757575"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road.highway",
+                    "elementType": "geometry",
+                    "stylers": [
+                        {
+                            "color": "#dadada"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road.highway",
+                    "elementType": "labels.text.fill",
+                    "stylers": [
+                        {
+                            "color": "#616161"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road.highway.controlled_access",
+                    "elementType": "geometry.fill",
+                    "stylers": [
+                        {
+                            "color": "#0a387a"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road.local",
+                    "elementType": "labels.text.fill",
+                    "stylers": [
+                        {
+                            "color": "#9e9e9e"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "transit.line",
+                    "elementType": "geometry",
+                    "stylers": [
+                        {
+                            "color": "#e5e5e5"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "transit.station",
+                    "elementType": "geometry",
+                    "stylers": [
+                        {
+                            "color": "#eeeeee"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "water",
+                    "elementType": "geometry",
+                    "stylers": [
+                        {
+                            "color": "#c9c9c9"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "water",
+                    "elementType": "labels.text.fill",
+                    "stylers": [
+                        {
+                            "color": "#9e9e9e"
+                        }
+                    ]
+                }
+            ],
+        {name: 'Styled Map'});
+         var map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: 47.430176, lng: 9.392557},
+            zoom: 14,
+            disableDefaultUI: true,
+        });
+          
+        var image = 'img/marker.png';
+        var falkensteinMarker = new google.maps.Marker({
+            position: {lat: 47.430176, lng: 9.392557},
+            map: map,
+            icon: image,
+        });
+        map.mapTypes.set('styled_map', styledMapType);
+        map.setMapTypeId('styled_map');
+        }
+    </script>
    
     
     <?php include 'include/footer.php'; ?>
@@ -49,6 +166,8 @@
     <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.3.1.min.js"><\/script>')</script>
     <script src="js/plugins.js"></script>
     <script src="js/main.js"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCaMyYyCYt5gnKomtPgBPCFKkPoNTdgtK8&callback=initMap"
+    async defer></script>
     
  
 </body>
