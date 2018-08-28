66f461df9dee3fc545ff2c0f31aa3bcbbb5dad2b
timbuk <ivoschwizer@gmail.com>
2018-08-28 11:41:20 +0200

    finale

--- advokatur.php
+++ advokatur.php
@@ -11,7 +11,7 @@
     
     <div class="main-container">
         <article class="content-top column-five">
-            <h1><span class="contact-resp">Advokatur <span class="respbr"></span>am Falkenstein</span><span class="respbr "></span><br><span class="gray">Wir sind ein engagiertes und kompetentes Team von Rechtsanwälten und Notaren, die sich für die Rechte unserer Klienten einsetzen. <span class="wl">weiter lesen</span><span class="wl-hidden">Wir beraten und vertreten unsere Klienten auch vor Gerichten und Verwaltungsbehörden. Spezialfragen und ausserordentliche Problemstellungen unterbreiten wir unserm Experten-Netz und holen auch ohne Prestige-Verlust eine Zweitmeinung ein.</span></span></h1>
+            <h1><span class="contact-resp">Advokatur <span class="respbr"></span>am Falkenstein</span><span class="respbr "></span><br><span class="gray">Als engagierte und kompetente Kanzleigemeinschaft von selbständigen Rechtsanwälten und Notaren setzen wir uns zielorientiert, effizient und diskret für Ihre Rechte ein. <span class="wl">weiter lesen</span><span class="wl-hidden">Sollte eine gütliche Lösung nicht möglich sein, vertreten wir Ihre Interessen vor Gerichten und Verwaltungsbehörden. Sie profitieren von der mehrjährigen Berufserfahrung, der laufenden Weiterbildung und der interdisziplinären Vernetzung der Anwälte.</span></span></h1>
         </article>
         
         <article>
@@ -70,18 +70,51 @@
                 </h3>
             </a>
             <article class="content-left column-five inview">
-                <h2><span class="contact-resp">Unser Team in der Advokatur</span><span class="respbr"></span><br><span class="gray">deckt die Bedürfnisse und Probleme, welche sich im KMU-Bereich und in der Familie wie auch im Beruf stellen, umfassend ab. Mit interner Kooperation und Konsultation analysieren wir die Sach- und Rechtslage und finden so das adäquate Vorgehen – gerade auch unter dem Aspekt der gewaltigen Kosten, welche die Gerichte heute den Parteien auferlegen. Transparent und verständlich für den Klienten, konsequent und wenn notwendig hartnäckig gegenüber dem Gegner und den Behörden, verfolgen wir die Interessen unseres Auftraggebers.</span></h2>
+                <h2><span class="gray">Aufgrund der unterschiedlichen Spezialisierungen der Anwälte können wir Ihnen in sämtlichen Lebensbereichen kompetente Beratung bieten. Wir decken umfassend sämtliche Rechtsgebiete ab, mit denen Sie in Alltag und Beruf in Berührung kommen. Wenn Ihr Anliegen bei den nachfolgenden Beispielen nicht aufgeführt ist, zögern Sie nicht, uns telefonisch anzufragen.</span></h2>
             </article>
         </article>
-        <article class="column-three accordion accordion-advo inview">
-            <h3 class="nav accTitle"><span class="line">&#x0007C;</span>Familienrecht</h3>
-                <p class="accList gray">Deckt die Bedürfnisse und Probleme, welche sich im KMU-Bereich und in der Familie wie auch im Beruf stellen, umfassend ab. Mit interner Kooperation und Konsultation analysieren wir die Sach- und Rechtslage und finden so das adäquate Vorgehen gerade auch unter dem Aspekt der gewaltigen Kosten, welche die Gerichte heute den Parteien auferlegen. Transparent und verständlich für den Klienten, konsequent und wenn notwendig hartnäckig gegenüber dem Gegner und den Behörden, verfolgen wir die Interessen unseres Auftraggebers.
+        <article class="column-four accordion accordion-advo inview">
+            <h3 class="nav accTitle"><span class="line">&#x0007C;</span>Vertragsrecht</h3>
+                <p class="accList gray">Wir gestalten Ihre Verträge und helfen dadurch vorbeugend, Streitigkeiten zu verhindern. Wenn sich diese nicht vermeiden lassen, vertreten wir Ihre Interessen gegenüber der Gegenpartei und wenn notwendig vor Gericht. Nicht nur die gesetzlich geregelten Verträge (Aufträge, Kauf-, Miet-, Arbeits-, Darlehens- oder Werkverträge, etc.) werden dem Vertragsrecht zugeordnet, sondern bspw. auch Konkubinats- oder Leasingverträge.
                 </p>
-            <h3 class="nav accTitle"><span class="line">&#x0007C;</span>Familienrecht</h3>
-                <p class="accList gray">Deckt die Bedürfnisse und Probleme, welche sich im KMU-Bereich und in der Familie wie auch im Beruf stellen, umfassend ab. Mit interner Kooperation und Konsultation analysieren wir die Sach- und Rechtslage und finden so das adäquate Vorgehen gerade auch unter dem Aspekt der gewaltigen Kosten, welche die Gerichte heute den Parteien auferlegen. Transparent und verständlich für den Klienten, konsequent und wenn notwendig hartnäckig gegenüber dem Gegner und den Behörden, verfolgen wir die Interessen unseres Auftraggebers.
+            <h3 class="nav accTitle"><span class="line">&#x0007C;</span>Familienrecht mit Kindes- und Erwachsenenschutzrecht</h3>
+                <p class="accList gray">Wir beraten Sie bei der Erstellung von Eheverträgen, um bspw. den optimalen Güterstand für Sie als Paar zu finden. Als Notare suchen und beurkunden wir die für Sie beste und individuell angepasste Lösung.<br><br>Eine Trennung und/oder Scheidung ist immer eine schwierige Lebensphase. Wir führen Sie fachkundig und erfahren durch diese Krise. Wenn es sein muss, leiten wir die nötigen Verfahren ein, wie bspw. Eheschutzverfahren und/oder andere vorsorgliche Massnahmen. Auf Wunsch beider Ehepartner erarbeiten wir eine Trennungs- oder Scheidungsvereinbarung im Sinne einer fairen, schnellen und kostengünstigen Lösung. In einer streitigen Scheidung vertreten wir Sie als einzelnen Ehepartner und setzen Ihre Ansprüche für Sie durch. Sind Kinderbelange, wie z.B. das Besuchsrecht umstritten, suchen wir mit Ihnen und den involvierten Stellen eine Lösung zum Wohle der betroffenen Kinder.
                 </p>
-            <h3 class="nav accTitle"><span class="line">&#x0007C;</span>Familienrecht</h3>
-                <p class="accList gray">Deckt die Bedürfnisse und Probleme, welche sich im KMU-Bereich und in der Familie wie auch im Beruf stellen, umfassend ab. Mit interner Kooperation und Konsultation analysieren wir die Sach- und Rechtslage und finden so das adäquate Vorgehen gerade auch unter dem Aspekt der gewaltigen Kosten, welche die Gerichte heute den Parteien auferlegen. Transparent und verständlich für den Klienten, konsequent und wenn notwendig hartnäckig gegenüber dem Gegner und den Behörden, verfolgen wir die Interessen unseres Auftraggebers.
+            <h3 class="nav accTitle"><span class="line">&#x0007C;</span>Sozial- und Privatversicherungsrecht mit Haftpflichtrecht</h3>
+                <p class="accList gray">Bei einem Unfall oder einer Krankheit werden Sie rasch mit verschiedenen Versicherern konfrontiert. Wir helfen Ihnen, Ihre Ansprüche abzuklären und gegenüber den jeweiligen Sozial- und Privatversicherungen geltend zu machen und nötigenfalls gerichtlich durchzusetzen.<br><br>Falls Sie durch einen Dritten verletzt oder Ihr Eigentum beschädigt wurde, prüfen wir Ihre Ansprüche gegenüber diesem Dritten oder dessen Versicherungen und unterstützen Sie bei der Durchsetzung Ihrer Forderung. Umgekehrt helfen wir Ihnen, unberechtigte Ansprüche abzuwehren.<br><br>Frau RA lic. iur. Christa Rempfler und Frau RA lic. iur. HSG Debora Bilgeri verfügen in diesem Bereich über ausgewiesene langjährige Berufserfahrung.
+                </p>
+            <h3 class="nav accTitle"><span class="line">&#x0007C;</span>Erbrecht und Vorsorge</h3>
+                <p class="accList gray">Wir zeigen Ihnen die Möglichkeiten auf, wie Sie Ihren Nachlass mit einem Testament oder Erbvertrag regeln können. Als Notare erstellen wir Ihnen die entsprechenden öffentlichen Urkunden. Wir vertreten Ihre Interessen bei sämtlichen Streitigkeiten im Zusammenhang mit einer Erbschaft und bemühen uns, nach Möglichkeit zeit- und kostenintensive Prozesse zu verhindern. Sollte dies nicht möglich sein, helfen wir Ihnen bei der gerichtlichen Durchsetzung Ihrer Ansprüche.<br><br>Situationsbezogen empfiehlt sich das Verfassen einer Patientenverfügung und/oder eines Vorsorgeauftrages. Wir unterstützen Sie, Ihren Willen schriftlich festzuhalten und sorgen für die rechtlich korrekte Form.
+                </p>
+            <h3 class="nav accTitle"><span class="line">&#x0007C;</span>Pharma-, Medizinal- und Güterkontrollrecht</h3>
+                <p class="accList gray">RA Dr. iur. Frank Th. Petermann ist auf diesem Gebiet der Fachmann mit ausgewiesener langjähriger Erfahrung.
+                </p>
+            <h3 class="nav accTitle"><span class="line">&#x0007C;</span>Strafrecht</h3>
+                <p class="accList gray">Falls Sie von einer Straftat betroffen oder in ein Strafverfahren involviert sind, vertreten wir Sie gegenüber der Polizei, Staatsanwaltschaft sowie den Gerichten. Wir machen als Opfer- und Geschädigtenvertreter Ihre Ansprüche im Strafverfahren geltend und haben dabei auch bereits das allfällig folgende Haftpflichtverfahren im Blick.
+                </p>
+            <h3 class="nav accTitle"><span class="line">&#x0007C;</span>Rufschädigungen und Europäische Menschenrechtskonvention (EMRK)</h3>
+                <p class="accList gray">Werden Sie bspw. im Internet mit unwahren Behauptungen angegriffen oder einfach &laquo;nur&raquo; beschimpft, so verfügt insbesondere RA lic. iur. HSG Rolf W. Rempfler über ausgewiesene langjährige Erfahrung in der Geltendmachung von sogenannten Persönlichkeits- und Ehrverletzungen. Auch EMRK-Beschwerdeverfahren gehören zu seinem Spezialgebiet.
+                </p>
+            <h3 class="nav accTitle"><span class="line">&#x0007C;</span>Strassenverkehrsrecht</h3>
+                <p class="accList gray">Ein Verkehrsunfall oder eine Geschwindigkeitsübertretung kann wie jede Verkehrsregelverletzung zusätzlich zur Busse einen Führerausweisentzug nach sich ziehen. Wir vertreten Ihre Interessen sowohl im Straf- als auch Administrativmassnahmeverfahren und setzen uns dafür ein, dass Strafe wie auch Entzugsdauer möglichst gering ausfallen, wenn sie unvermeidbar sein sollten. Ferner machen wir Ihre Ansprüche gegenüber Versicherungen oder einem allfälligen Unfallverursacher geltend. In diesem Bereich verfügt insbesondere Frau RA lic. iur. HSG Debora Bilgeri über ausgewiesene langjährige Erfahrung.
+                </p>
+            <h3 class="nav accTitle"><span class="line">&#x0007C;</span>Betreibungs- und Konkursrecht</h3>
+                <p class="accList gray">Die Durchsetzung von Geldforderungen kann mühsam und zeitaufwendig sein. Wir helfen Ihnen, Ihre Schuldner ins Recht zu fassen und Ihre Ansprüche wenn nötig vor Gericht geltend zu machen und vollstrecken zu lassen. Dabei achten wir darauf, dass unser Vorgehen nicht nur rechtlich, sondern auch wirtschaftlich Sinn macht.<br><br>Als Schuldnervertreter beraten wir Sie umfassend, damit Sie Ihre Vermögenslage wieder in den Griff bekommen und nachhaltig schuldenfrei werden können.
+                </p>
+            <h3 class="nav accTitle"><span class="line">&#x0007C;</span>Firmengründung und -führung</h3>
+                <p class="accList gray">Möchten Sie eine Firma gründen oder Ihr Unternehmen umstrukturieren? Die Wahl der richtigen Rechtsform für ein Start-Up oder KMU ist für viele kostenrelevante Faktoren entscheidend. Wir beraten Sie umfassend und nehmen die Gründung oder Umstrukturierung für Sie vor.<br><br>Wir begleiten Sie beim Aufbau der Firma mit allen damit verbundenen rechtlichen Aufgaben, wie bspw. die Abfassung von Allgemeinen Geschäftsbedingungen (AGB), der Abschluss von Versicherungen, etc. Bei Bedarf kann die Treuhand am Falkenstein beigezogen werden.
+                </p>
+            <h3 class="nav accTitle"><span class="line">&#x0007C;</span>Verwaltungs-, Bau- und Planungsrecht, Sachenrecht (Dienstbarkeiten, etc.)</h3>
+                <p class="accList gray">Aufgrund der engen Zusammenarbeit mit der Treuhand am Falkenstein in unserem Geschäftszentrum können wir Ihnen eine rechtlich und wirtschaftlich fundierte Beratung im Zusammenhang mit Grundstücken anbieten, vom Kauf oder Bau einer Liegenschaft inkl. Planung und Bewilligung des Bauprojekts bis zum Verkauf. Treten Mängel auf, können wir Ihnen helfen, Ihren Schaden möglichst klein zu halten. Falls Sie Ihre Liegenschaft nicht selbst verwalten möchten, übernimmt dies die Immobilienverwaltung Treuhand am Falkenstein gerne für Sie. Bei Problemen mit dem Nachbarn oder bspw. mit einer Grunddienstbarkeit vertreten wir Ihre Interessen im vorgerichtlichen Verfahren wie auch in einem allfälligen Prozess.
+                </p>
+            <h3 class="nav accTitle"><span class="line">&#x0007C;</span>Steuerrecht</h3>
+                <p class="accList gray">Möchten Sie Ihre Steuerlast optimieren oder haben Sie ein fiskalisches Problem? Wir helfen Ihnen gerne. Auf Wunsch können wir die Treuhand am Falkenstein hinzuziehen.
+                </p>
+            <h3 class="nav accTitle"><span class="line">&#x0007C;</span>Haben Sie Probleme im Kontakt mit einer Behörde?</h3>
+                <p class="accList gray">Benötigen Sie Unterstützung im Umgang mit z.B. der Migrationsbehörde oder benötigen Sie eine Bewilligung, bspw. für eine Veranstaltung? Wir beraten und vertreten Sie im ganzen Verwaltungsbereich.
+                </p>
+            <h3 class="nav accTitle"><span class="line">&#x0007C;</span>Haben Sie ein anderes juristisches Problem oder möchten Sie eine unverbindliche Beratung?</h3>
+                <p class="accList gray">Kontaktieren Sie unser Sekretariat telefonisch unter 071 242 66 50 oder auf folgender E-Mail-Adresse: info@falkenstein.ag. Sollten wir Ihnen nicht weiterhelfen können, so empfehlen wir Ihnen gerne einen dafür geeigneten Berufskollegen.
                 </p>
         </article>
     </div>
