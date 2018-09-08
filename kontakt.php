a519cd19916198db2602fccdf35a98aeccff7420
timbuk <ivoschwizer@gmail.com>
2018-09-08 10:15:27 +0200

    map live

--- kontakt.php
+++ kontakt.php
@@ -40,41 +40,31 @@
     
     <script>
         function initMap() {
-        var styledMapType = new google.maps.StyledMapType(
-            [{"elementType": "geometry","stylers": [{"color": "#f5f5f5"}]},
-             {"elementType": "labels.icon","stylers": [{"visibility": "off"}]},
-             {"elementType": "labels.text.fill","stylers": [{"color": "#616161"}]},
-             {"elementType": "labels.text.stroke","stylers": [{"color": "#f5f5f5"}]},
-             {"featureType": "administrative.land_parcel","elementType": "labels.text.fill","stylers": [{"color": "#bdbdbd"}]},
-             {"featureType": "poi","elementType": "geometry","stylers": [{"color": "#eeeeee"}]},{"featureType": "poi","elementType": "labels.text.fill","stylers": [{"color": "#757575"}]},
-             {"featureType": "poi.park","elementType": "geometry","stylers": [{"color": "#e5e5e5"}]},{"featureType": "poi.park","elementType": "labels.text.fill","stylers": [{"color": "#9e9e9e"}]},
-             {"featureType": "road","elementType": "geometry","stylers": [{"color": "#ffffff"}]},{"featureType":"road.arterial","elementType": "labels.text.fill","stylers": [{"color": "#757575"}]},{"featureType": "road.highway","elementType": "geometry","stylers": [{"color": "#dadada"}]},
-             {"featureType": "road.highway","elementType": "labels.text.fill","stylers": [{"color": "#616161"}]},
-             {"featureType": "road.highway.controlled_access","elementType": "geometry.fill","stylers": [{"color": "#0a387a"}]},
-             {"featureType": "road.local","elementType": "labels.text.fill","stylers": [{"color": "#9e9e9e"}]},
-             {"featureType": "transit.line","elementType": "geometry","stylers": [{"color": "#e5e5e5"}]},{"featureType": "transit.station", "elementType": "geometry", "stylers": [{"color": "#eeeeee" }]},
-             {"featureType": "water","elementType": "geometry","stylers": [{"color": "#c9c9c9"}]},
-             {"featureType": "water","elementType": "labels.text.fill","stylers": [{"color": "#9e9e9e"}]}
-           ],
-        {name: 'Styled Map'});
-         var map = new google.maps.Map(document.getElementById('map'), {
-            center: {lat: 47.430176, lng: 9.392557},
-            zoom: 13,
-            disableDefaultUI: true,
-        });
-        
-        var image = {
-            url: 'img/marker.svg',
-            scaledSize: new google.maps.Size(32, 44),
-        }
-         var falkensteinMarker = new google.maps.Marker({
-            position: {lat: 47.430176, lng: 9.392557}, 
-            map: map,
-            optimized: false,
-            icon: image,
-        });
-        map.mapTypes.set('styled_map', styledMapType);
-        map.setMapTypeId('styled_map');
+            var falkenstein = {lat: 47.429926, lng: 9.391524}; 
+            var map = new google.maps.Map(
+                document.getElementById('map'), {
+                    zoom: 14, 
+                    center: falkenstein,
+                    zoomControl: undefined,
+                    mapTypeControl: false,
+                    scaleControl: false,
+                    streetViewControl: false,
+                    rotateControl: false,
+                    fullscreenControl: false,
+                    styles: [
+                        {"featureType": "administrative","elementType": "geometry","stylers":[{"visibility": "off"}]},
+                        {"featureType": "landscape.man_made","elementType": "geometry.stroke","stylers": [{"color": "#c8c8c8"}]},
+                        {"featureType": "poi","stylers": [{"visibility": "off"}]},
+                        {"featureType": "road","elementType": "labels.icon","stylers": [{"visibility": "off"}]},
+                        {"featureType": "transit","stylers": [{"visibility": "off"}]}
+                    ]
+                });
+            
+            var marker = new google.maps.Marker({
+                position: falkenstein, 
+                map: map,
+                icon: 'img/marker.svg',
+            });
         }
     </script>
    
