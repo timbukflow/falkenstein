473e7b7e81af6a4c4010106b0bd83412bcf65265
timbuk <ivoschwizer@gmail.com>
2018-09-08 08:53:44 +0200

    map fix

--- kontakt.php
+++ kontakt.php
@@ -70,7 +70,7 @@
          var falkensteinMarker = new google.maps.Marker({
             position: {lat: 47.430176, lng: 9.392557}, 
             map: map,
-            
+            optimized: false,
             icon: image,
         });
         map.mapTypes.set('styled_map', styledMapType);
