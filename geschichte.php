a83d6a2c2207a6ab8848e290bec1b3549822706a
Ivo Schwizer <ivoschwizer@gmail.com>
2021-10-11 17:39:50 +0200

    update Geschichte

--- geschichte.php
+++ geschichte.php
@@ -7,56 +7,56 @@ am Falkenstein">
 </head>
 
 <body>
-    
+
     <?php $page = 'geschichte'; include 'include/header.php'; ?>
-    
+
     <div class="main-container">
         <article class="column-four storystyle storytop storystyle-left inview">
             <h4>1968 &#8211;<span class="gray">1983</span></h4>
                 <h2 class="gray story-bottom">Der Patron, lic. iur. Werner Rempfler, ist 1968 in die Advokatur eingestiegen. Nach den ersten 15 Jahren als intensiv tätiger Prozessanwalt konnte er 1983 die in der Jahrhundertwende während der St. Galler Textilblüte erbaute Eckliegenschaft an der Falkensteinstrasse 1 in St. Gallen-St. Fiden käuflich erwerben.
                 </h2>
-                
+
         </article>
         <article class="column-four storystyle inview">
             <h4>1983 &#8211;<span class="gray">1996</span></h4>
                 <h2 class="gray story-bottom">Schritt um Schritt hat er die Immobilie, die beim Kauf noch unverändert den Stand von 1900 aufwies, auf den neuesten Stand gebracht. Das &laquo;Milchlädeli&raquo; im Erdgeschoss mutierte zum Ladengeschäft, die Wohnung im 1. OG wurde zum Anwaltsbüro. Im Jahre 1996 erfolgte der Ausbau des Daches zum zweigeschossigen weiteren Büro.
                 </h2>
         </article>
-        
+
         <div class="column-six content-story-img">
             <div class="container-img inview">
                 <img class="img1" src="img/falkenstein-advokatur-geschichte-a.jpg" alt="falkenstein-advokatur-treuhand-home" >
                 <img class="img2 mobile" src="img/falkenstein-advokatur-geschichte-b.jpg" alt="falkenstein-advokatur-treuhand-home" >
             </div>
-        </div> 
-        
+        </div>
+
         <article class="column-four storystyle storystyle-left inview">
             <h4>1999</h4>
                 <h2 class="gray story-bottom">Im letzten Jahr des alten Jahrtausends erfuhr die Kanzlei ihre erste personelle Erweiterung, als RA Dr. Petermann als Kanzleipartner hinzukam; womit einerseits weitere spezialisierte Fachbereiche abgedeckt werden konnten und die Kanzlei organisatorisch neu aufgestellt wurde.
                 </h2>
         </article>
-        
+
         <article class="column-four storystyle inview">
             <h4>2005 &#8211;<span class="gray">2007</span></h4>
                 <h2 class="gray story-bottom">Die ersten beiden Anwaltspraktikanten der Kanzlei, RA Rolf W. Rempfler und RA Christa Rempfler, kamen als neue Partner hinzu.
                 </h2>
         </article>
-         
+
         <article class="column-four storystyle storystyle-left inview">
             <h4>2017</h4>
                 <h2 class="gray story-bottom">RA Andres Thürlimann erweitert als selbständiger Rechtsanwalt die Kanzleigemeinschaft.
                 </h2>
         </article>
-        
+
         <article class="column-four storystyle inview">
             <h4>2018</h4>
                 <h2 class="gray story-bottom">Im 50. Jubiläumsjahr der Kanzlei verstärken RA Debora Bilgeri als selbständige Rechtsanwältin und RA Tanja Ivanovic das Team. Das Geschäftszentrum mit dem Advokatur- und Treuhandbüro startet auf vier Geschossen mit sechs Rechtsanwälten und sechs Mitarbeitern in die nächsten 50 Jahre. Im schmuck renovierten Geschäftshaus möge dem Team und seinen Klienten und Kunden Glück und Erfolg beschieden sein, getreu der Marmortafel am Haus: &laquo;iustitia vincit&raquo;.
                 </h2>
         </article>
-        
+
         <article class="column-four storystyle storystyle-left inview">
             <h4>2019</h4>
-                <h2 class="gray story-bottom">Am 12. April 2019, dem 20. Jahrestag seines Eintritts in die Kanzlei, hat RA Frank Th. Petermann seinen Austritt aus der Advokatur am Falkenstein angekündigt. Er wird künftig ausschliesslich beratend tätig sein. In den kommenden Jahren wird er langjährigen Klienten bei der Implementierung der neuen Kontrollparameter bzgl. der Kompensationsmesstechniken von Werkzeugmaschinen behilflich sein. Daneben wird er sich künftig auch vermehrt der Fachpublizistik und der Metallbildhauerei zuwenden. Für seine wissenschaftlichen Publikationen wird er weiterhin die Infrastruktur der Kanzlei nutzen. Sein privates Sekretariat wird wie bis anhin in der Kanzlei geführt.
+                <h2 class="gray story-bottom">Am 12. April 2019, dem 20. Jahrestag seines Eintritts in die Kanzlei, hat RA Frank Th. Petermann seinen Austritt aus der Advokatur am Falkenstein angekündigt. Er wird künftig ausschliesslich beratend tätig sein und bleibt uns daher nicht nur als Konsiliarius in seinen bevorzugten Fachgebieten, sondern auch weiterhin als Verwaltungsratspräsident der Geschäftszentrum Falkenstein AG erhalten. Daneben wird er sich wieder vermehrt der Publizistik und der Metallbildhauerei zuwenden. Sein privates Sekretariat wird wie bis anhin in der Kanzlei geführt werden.
                 </h2>
         </article>
         <article class="column-four storystyle inview">
@@ -64,25 +64,25 @@ am Falkenstein">
                 <h2 class="gray story-bottom">RA Matthias Burri-Küng verstärkt als selbständiger Rechtsanwalt die Kanzleigemeinschaft.
                 </h2>
         </article>
-        
+
         <div class="column-six content-story-img">
             <div class="container-img inview">
                 <img class="img1" src="img/falkenstein-advokatur-haus-a.jpg" alt="falkenstein-advokatur-iustitia-vincit" >
                 <img class="img2 mobile" src="img/falkenstein-advokatur-haus-b.jpg" alt="falkenstein-advokatur-iustitia-vincit" >
             </div>
         </div>
-        
+
     </div>
-    
+
     <?php include 'include/footer.php'; ?>
-    
-    
+
+
     <script src="js/vendor/modernizr-3.6.0.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
     <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.3.1.min.js"><\/script>')</script>
     <script src="js/plugins.js"></script>
     <script src="js/main.js"></script>
- 
+
 </body>
 
 </html>
