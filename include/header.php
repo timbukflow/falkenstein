922aa4e60c6ce9c01dfb13b021346fae967f6fd2
Ivo Schwizer <ivoschwizer@gmail.com>
2022-03-01 07:41:09 +0100

    debora löschen

--- include/header.php
+++ include/header.php
@@ -1,23 +1,22 @@
 <header>
     <nav class="topnav">
-        
+
         <div class="menusmall" id="scrollable"><span class="line">&#x0007C;</span>Menu</div>
-        
+
         <div class="column-one navresp">
             <a href="advokatur" target="_top"><span class="line <?php if ($page=='advokatur'){echo 'lineactive';}?>">&#x0007C;</span>Advokatur</a>
         </div>
-        
+
         <div class="column-one navresp">
             <a href="treuhand" target="_top"><span class="line <?php if ($page=='treuhand'){echo 'lineactive';}?>">&#x0007C;</span>Treuhand</a>
         </div>
-        
+
         <div class="column-one navresp">
             <div class="menu dropTitle"><span class="line <?php if ($page=='team'){echo 'lineactive';}?>">&#x0007C;</span>Team</div>
             <div class="dropList subnav-resp">
                 <p class="subnav-titel"><span class="lineactive">&#x0007C;</span>Advokatur</p>
                 <a href="christa-rempfler" target="_top"><span class="line">&#x0007C;</span>Christa Rempfler</a>
                 <a href="rolf-w-rempfler" target="_top"><span class="line">&#x0007C;</span>Rolf W. Rempfler</a>
-                <a href="debora-bilgeri" target="_top"><span class="line">&#x0007C;</span>Debora Bilgeri</a>
                 <a href="andres-thuerlimann" target="_top"><span class="line">&#x0007C;</span>Andres Thürlimann</a>
                 <a href="tanja-ivanovic" target="_top"><span class="line">&#x0007C;</span>Tanja Ivanovic</a>
                 <a href="matthias-burri-kueng" target="_top"><span class="line">&#x0007C;</span>Matthias Burri-Küng</a>
@@ -30,20 +29,20 @@
                 <a href="fabienne-frei" target="_top"><span class="line">&#x0007C;</span>Fabienne Frei</a>
             </div>
         </div>
-        
+
         <div class="column-one navresp">
             <a href="geschichte" target="_top"><span class="line <?php if ($page=='geschichte'){echo 'lineactive';}?>">&#x0007C;</span>Geschichte</a>
         </div>
-        
+
         <div class="column-one navresp">
             <a href="kontakt" target="_top"><span class="line <?php if ($page=='kontakt'){echo 'lineactive';}?>">&#x0007C;</span>Kontakt</a>
         </div>
-        
+
         <div class="logo-container">
             <a href="index" target="_top">
                 <img class="logo-small" alt="advokatur-treuhand-falkenstein-logo-small" src="img/advokatur-treuhand-falkenstein-logo-small.svg">
             </a>
         </div>
-        
+
     </nav>
 </header>
