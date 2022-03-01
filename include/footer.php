ef4fca1e150ee472aea369cc52ee2cc82c725b51
Ivo Schwizer <ivoschwizer@gmail.com>
2022-03-01 07:45:42 +0100

    footer

--- include/footer.php
+++ include/footer.php
@@ -6,13 +6,13 @@
             Advokatur und Treuhand am Falkenstein<br>
             Falkensteinstrasse 1<br>
             CH-9000 St.Gallen<br><br>
-            
+
             Design &amp; Code:<br>
             Schwizer &amp; Schlatter GmbH<br>
             Davidstrasse 45<br>
             CH-9000 St.Gallen<br>
             <a href="https://schwizerschlatter.com/" target="_top">www.schwizerschlatter.com</a><br><br>
- 
+
             Fotografie:<br>
             Anna-Tina Eberhard Fotografie<br>
             Konkordiastrasse 23<br>
@@ -24,12 +24,12 @@
         <p class="footer accfooter" data-toggle="#disclaimer"><span class="line">&#x0007C;</span>Disclaimer</p>
         <p class="footer-content acclistFooter" id="disclaimer">
             Die Website www.falkenstein.ag dient der Informationsvermittlung über unsere Unternehmung sowie über unsere Produkte und Dienstleistungen.<br><br>
- 
+
             Der Sicherheit und Störungsfreiheit sowie der Aktualität bei der Benutzung der Website wurde grosse Aufmerksamkeit geschenkt. Sie wird regelmässig technisch auf den neuesten Stand gebracht und inhaltlich aktualisiert. Trotz bestmöglicher Vorkehrungen können Sicherheit und Störungsfreiheit sowie Aktualität und Vertraulichkeit von Daten und/oder Inhalten aufgrund des öffentlichen Charakters des Internets nicht abschliessend garantiert werden. Sie benutzen das Internet bzw. die von uns publizierten Webseiten grundsätzlich auf eigenes Risiko. Dies gilt auch für E-Mails [elektronische Post] sowie Links, die Sie zu uns führen, bzw. für Links, die wir Ihnen anbieten, sowie für den Inhalt der via Links aufgerufenen Websites von Dritten. Die Advokatur und Treuhand am Falkenstein übernimmt keinerlei Haftung für direkte oder indirekte Schäden, die den Nutzern der Webseiten oder Dritten durch irgendwelche Kontakte oder Transaktionen über das Internet entstehen; sie haften insbesondere nicht für Richtigkeit, Aktualität und Vollständigkeit der angezeigten bzw. telefonisch und elektronisch übermittelten Daten bzw. Inhalte. Eine Haftung wird namentlich [aber nicht abschliessend] ausgeschlossen für Übermittlungsfehler, technische Mängel, Störungen oder Unterbrechungen des Telefonnetzes/Internetzuganges, rechtswidrige Eingriffe in Einrichtungen der Netze, Einschmuggeln von Viren, Kopieren und Verfälschen von Daten und Inhalten, Überlastung der Netze, Nichterkennen von Fälschungen, allfällige Legitimationsmängel sowie mutwillige oder zufällige Blockierung elektronischer Zugänge durch Dritte.<br><br>
- 
+
             Alle Rechte vorbehalten. Jede Art der Vervielfältigung, Verbreitung, Auswertung oder Veränderung – auch auszugsweise – ist ohne vorgängige schriftliche Genehmigung durch die Advokatur und Treuhand am Falkenstein untersagt.<br><br>
- 
-            © 2021 Advokatur und Treuhand am Falkenstein. All rights reserved.
+
+            © 2022 Advokatur und Treuhand am Falkenstein. All rights reserved.
         </p>
     </div>
 </footer>
