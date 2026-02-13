f82c4bde1279b1fac52ddcbf4fb8efa9b659f79d
Ivo Schwizer <ivoschwizer@gmail.com>
2026-02-13 17:53:51 +0100

    update

--- include/head.php
+++ include/head.php
@@ -1,13 +1,11 @@
+    <?php $asset_version = '1.0'; ?>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="robots" content="index, follow">
     <meta name="author" content="Advokatur und Treuhand am Falkenstein">
-    
+
     <meta name="google-site-verification" content="a1HQiYpPonscftm-7z0OYnCmVsOfnUlEfpR6gpgd0fg" />
-    <meta http-equiv="cache-control" content="max-age=0" />
-    <meta http-equiv="cache-control" content="no-cache" />
-    <meta http-equiv="expires" content="0" />
-    <meta http-equiv="pragma" content="no-cache" />
+    <base href="https://falkenstein.ag/">
 
     <meta name="keywords" content="Advokatur, Treuhand, Immobilien, Falkenstein, Frank Th. Petermann, Rolf W. Rempfler, Christa Rempfler, Tanja Ivanovic, Werner Rempfler, Maria Rempfler, Roger Schai, Betreibungs- und Konkursrecht, Erbrecht und Vorsorge, Exportkontrollrecht, Familienrecht mit Kindes- und Erwachsenenschutzrecht, Firmengründung und -führung, Medizinalrecht, Pharmarecht, Rufschädigungen und Europäische Menschenrechtskonvention (EMRK), Sozial- und Privatversicherungsrecht mit Haftpflichtrecht, Steuerrecht, Strafrecht, Strassenverkehrsrecht, Vertragsrecht, Verwaltungs-, Bau- und Planungsrecht, Sachenrecht (Dienstbarkeiten, etc.),">
     <meta name="apple-mobile-web-app-status-bar-style" content="black">
@@ -39,7 +37,7 @@
     <meta name="msapplication-TileColor" content="#ffffff">
     <meta name="theme-color" content="#ffffff">
     
-    <link rel="stylesheet" href="css/normalize.css">
-    <link rel="stylesheet" href="css/main.css">
-    <link href="https://fonts.googleapis.com/css?family=PT+Sans" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=PT+Serif" rel="stylesheet">
+    <link rel="stylesheet" href="css/normalize.css?v=<?php echo $asset_version; ?>">
+    <link rel="stylesheet" href="css/main.css?v=<?php echo $asset_version; ?>">
+    <link href="https://fonts.googleapis.com/css?family=PT+Sans&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=PT+Serif&display=swap" rel="stylesheet">
--- kontakt.php
+++ kontakt.php
@@ -79,8 +79,8 @@
     <script src="js/vendor/modernizr-3.6.0.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
     <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.3.1.min.js"><\/script>')</script>
-    <script src="js/plugins.js"></script>
-    <script src="js/main.js"></script>
+    <script src="js/plugins.js?v=<?php echo $asset_version; ?>"></script>
+    <script src="js/main.js?v=<?php echo $asset_version; ?>"></script>
     <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyApYat9BIG_zCjqnDgkEpAg96BXeAQPA24&callback=initMap"
   type="text/javascript"></script>
  
